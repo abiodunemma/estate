84da5410a829463856d1a58f28b226bdcc5ab9ac
abiodun emma <abiodunemma769@gmail.com>
2024-11-11 22:51:00 +0100

    frontpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ const Header = ()  => {
   <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block
   max-w-3xl font-semibold pt-20'>Explore home that fit your dreams</h2>
     <div className='space-x-6 mt-16'>
-        <a href='P#' className='border border-white px-8
+        <a href='#Projects' className='border border-white px-8
         py-3 rounded'>Projects</a>
-        <a href='#' className='bg-blue-500 px-8
+        <a href='#Contact' className='bg-blue-500 px-8
         py-3 rounded'>Contact Us</a>
     </div>
 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 
 function Navbar() {
+  const [showMobileManu, setShowMobileManu] = useState(false)
+
+  useEffect(()=>{
+    if(showMobileManu){
+      document.body.style.overflow = 'hidden'
+    }else{
+       document.body.style.overflow = 'auto'
+    }
+    return ()=>{
+      document.body.style.overflow = 'auto'
+    };
+  }, [showMobileManu])
   return (
     <div className='absolute top-0 w-full z-10'>
 <div className='container mx-auto flex justify-between
@@ -20,7 +32,22 @@ bg-transparent'>
         hover:text-gray-400'>Testtimonials</a>
     </ul>
     <button className='hidden md:block bg-white px-8
-    py-2 rouned-full'>Sign up</button>
+    py-2 rounded-full'>Sign up</button>
+    <img onClick={()=> setShowMobileManu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt='' />
+</div>
+{/* -------- mobile menu------- */}
+<div className={`md:hidden ${showMobileManu ? 'fixed w-full' : 'h-0 w-0' }  right-0 top-0 bottom-0
+overflow-hidden bg-white transition-all`}>
+  <div className='flex justify-end p-6 cursor-pointer'>
+    <img onClick={()=> setShowMobileManu(false)} src={assets.cross_icon} className='w-6' alt='' />
+  </div>
+  <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg
+  font-medium'>
+    <a onClick={()=> setShowMobileManu(false)} href='#Header' className='px-4 py2 rounded-full inline-block'>Home</a>
+    <a onClick={()=> setShowMobileManu(false)} href='#About' className='px-4 py2 rounded-full inline-block'>About</a>
+    <a onClick={()=> setShowMobileManu(false)} href='#Projects' className='px-4 py2 rounded-full inline-block'>projects</a>
+    <a onClick={()=> setShowMobileManu(false)} href='#Testimonails' className='px-4 py2 rounded-full inline-block'>Testtimonials</a>
+  </ul>
 </div>
     </div>
   )
